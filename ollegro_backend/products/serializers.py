5462afa4d4d12ca0cc785a80fafa5c81398ca2ea
TRoll_94 <surnin2003@mail.ru>
2023-06-11 00:02:17 +0200

    Added validations to create\update product

--- ollegro_backend/products/serializers.py
+++ ollegro_backend/products/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -36,17 +37,40 @@ class ProductCreateSerializer(serializers.ModelSerializer):
         """
         validate
         """
-        products_with_same_sku_and_properties = Product.objects.filter(sku=data['sku'],
-                                                                       properties__in=data['properties'])
+        sku = data.get('sku')
+        category_id = data.get('category_id')
+        owner_id = data.get('owner_id')
+        properties = data.get('properties', [])
+
+        # Validate a product with the same SKU and properties
+        with_same_sku_and_properties = Product.objects.filter(sku=sku, properties__in=properties)
         if self.instance:
-            products_with_same_sku_and_properties = products_with_same_sku_and_properties.exclude(id=self.instance.id)
+            with_same_sku_and_properties = with_same_sku_and_properties.exclude(id=self.instance.id)
+        if with_same_sku_and_properties.exists():
+            raise serializers.ValidationError('Product with the same SKU and properties already exists.')
 
-        if products_with_same_sku_and_properties.exists():
-            raise ValidationError(detail='Product with the same SKU and properties already exists.')
-
+        # Check for duplicate property codes
         property_codes = [prop.code for prop in data.get('properties', [])]
         if len(property_codes) != len(set(property_codes)):
             raise serializers.ValidationError('Duplicate property codes found in the product.')
+
+        # Checking a product with the same SKU in different categories
+        with_difference_categories = Product.objects.filter(sku=sku).exclude(category_id=category_id)
+        if with_difference_categories.exists():
+            raise serializers.ValidationError('Duplicate SKU in different categories.')
+
+        # Checking a product with the same SKU from different owners
+        with_difference_users = Product.objects.filter(sku=sku).exclude(owner_id=owner_id)
+        if with_difference_users.exists():
+            raise serializers.ValidationError('Duplicate SKU among different users.')
+
+        # Check if properties belong to the selected category
+        properties_count = ProductProperty.objects.filter(id__in=properties).values('category_id').annotate(
+            count=Count('id'))
+        for property_count in properties_count:
+            if property_count['category_id'] != category_id or property_count['count'] != len(properties):
+                raise serializers.ValidationError(
+                    'One or more product properties do not belong to the selected category.')
 
         return data
 
